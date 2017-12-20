3c7fd5ff11892ea79a827184b9ae8a41eaa83889
lzy <1158600445@qq.com>
2017-12-20 16:39:55 +0800

    Initial commit

---
+++ src/main/java/com/springboot/controller/helloController.java
@@ -0,0 +1,19 @@
+package com.springboot.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by lzy on 2017/12/20.
+ */
+@RestController
+@EnableAutoConfiguration
+public class helloController {
+
+    @RequestMapping("/hello")
+    public String index(){
+        System.out.println("进入controller");
+        return "hello world!";
+    }
+}
